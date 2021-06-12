691c4dbda53c0b08467cff0c8345062466f6e388
pwoch <43825825+pwoch@users.noreply.github.com>
2021-06-12 17:40:00 +0200

    Basic weapon pick up system

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -4,87 +4,135 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    [SerializeField] private GameObject gun;
-    [SerializeField] private GameObject knife;
-    private GameObject activeWeapon;
-    private Gun gunComponent;
-    private Knife knifeComponent;
+    public GameObject weapon;
+    public Gun weaponComponent;
     private PlayerMovement playerMovement;
     void Start()
     {
         playerMovement = GetComponent<PlayerMovement>();
-        SetActiveWeapon(gun);
-        GetComponentOfActiveWeapon();
+        weaponComponent = weapon.GetComponent<Gun>();
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Attack();
+            Shoot();
         }
         if (Input.GetKeyDown(KeyCode.R))
         {
             Reload();
         }
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            SetActiveWeapon(gun);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            SetActiveWeapon(knife);
-        }
-    }
-
-    public void SetActiveWeapon(GameObject newWeaponPrefab)
-    {
-        if(activeWeapon != null)
-        {
-            Destroy(activeWeapon);
-        }
-        GameObject newWeapon = Instantiate(newWeaponPrefab);
-        newWeapon.transform.position = playerMovement.weaponGrip.transform.position;
-        newWeapon.transform.rotation = transform.rotation;
-        newWeapon.transform.SetParent(playerMovement.weaponGrip.transform);
-        activeWeapon = newWeapon;
-        GetComponentOfActiveWeapon();
     }
 
     public int GetAmmunition()
     {
-        return gunComponent != null ? gunComponent.ammunition : 0;
+        return weaponComponent.ammunition;
     }
 
     public int GetShotsBeforeReload()
     {
-        return gunComponent != null ? gunComponent.reloadCounter : 0;
+        return weaponComponent.reloadCounter;
     }
 
-    private void Attack()
+    private void Shoot()
     {
-        if (gunComponent != null)
-        {
-            gunComponent.Shoot();
-        }
-        else if(knifeComponent != null)
-        {
-            Vector3 direction = transform.forward;
-            knifeComponent.Attack(direction);
-        }
+        weaponComponent.Shoot();
     }
 
     private void Reload()
     {
-        if (gunComponent != null)
-        {
-            gunComponent.Reload();
-        }
-    }
-
-    private void GetComponentOfActiveWeapon()
-    {
-        gunComponent = activeWeapon.GetComponent<Gun>();
-        knifeComponent = activeWeapon.GetComponent<Knife>();
+        weapon.GetComponent<Gun>().Reload();
     }
 }
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class PlayerShooting : MonoBehaviour
+//{
+//    [SerializeField] private GameObject gun;
+//    [SerializeField] private GameObject knife;
+//    private GameObject activeWeapon;
+//    private Gun gunComponent;
+//    private Knife knifeComponent;
+//    private PlayerMovement playerMovement;
+//    void Start()
+//    {
+//        playerMovement = GetComponent<PlayerMovement>();
+//        SetActiveWeapon(gun);
+//        GetComponentOfActiveWeapon();
+//    }
+
+//    void Update()
+//    {
+//        if (Input.GetMouseButtonDown(0))
+//        {
+//            Attack();
+//        }
+//        if (Input.GetKeyDown(KeyCode.R))
+//        {
+//            Reload();
+//        }
+//        if (Input.GetKeyDown(KeyCode.Alpha1))
+//        {
+//            SetActiveWeapon(gun);
+//        }
+//        if (Input.GetKeyDown(KeyCode.Alpha2))
+//        {
+//            SetActiveWeapon(knife);
+//        }
+//    }
+
+//    public void SetActiveWeapon(GameObject newWeaponPrefab)
+//    {
+//        if(activeWeapon != null)
+//        {
+//            Destroy(activeWeapon);
+//        }
+//        GameObject newWeapon = Instantiate(newWeaponPrefab);
+//        newWeapon.transform.position = playerMovement.weaponGrip.transform.position;
+//        newWeapon.transform.rotation = transform.rotation;
+//        newWeapon.transform.SetParent(playerMovement.weaponGrip.transform);
+//        activeWeapon = newWeapon;
+//        GetComponentOfActiveWeapon();
+//    }
+
+//    public int GetAmmunition()
+//    {
+//        return gunComponent != null ? gunComponent.ammunition : 0;
+//    }
+
+//    public int GetShotsBeforeReload()
+//    {
+//        return gunComponent != null ? gunComponent.reloadCounter : 0;
+//    }
+
+//    private void Attack()
+//    {
+//        if (gunComponent != null)
+//        {
+//            gunComponent.Shoot();
+//        }
+//        else if(knifeComponent != null)
+//        {
+//            Vector3 direction = transform.forward;
+//            knifeComponent.Attack(direction);
+//        }
+//    }
+
+//    private void Reload()
+//    {
+//        if (gunComponent != null)
+//        {
+//            gunComponent.Reload();
+//        }
+//    }
+
+//    private void GetComponentOfActiveWeapon()
+//    {
+//        gunComponent = activeWeapon.GetComponent<Gun>();
+//        knifeComponent = activeWeapon.GetComponent<Knife>();
+//    }
+//}
---
+++ Assets/Scripts/Weapon/Pick.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pick : MonoBehaviour
+{
+    public Gun gunScript;
+    public GameObject weapon;
+    public Rigidbody rb;
+    public BoxCollider coll;
+    public Transform gunContainer, fpsCam;
+    public GameObject player;
+
+    public float pickUpRange;
+    public float dropForwardForce, dropUpwardForce;
+
+    public bool equipped;
+    public static bool slotFull;
+
+    private void Start()
+    {
+        //Setup
+        if (!equipped)
+        {
+            gunScript.enabled = false;
+            rb.isKinematic = false;
+            coll.isTrigger = false;
+        }
+        if (equipped)
+        {
+            gunScript.enabled = true;
+            rb.isKinematic = true;
+            coll.isTrigger = true;
+            slotFull = true;
+        }
+    }
+
+    private void Update()
+    {
+        //Check if player is in range and "E" is pressed
+        Vector3 distanceToPlayer = player.transform.position - transform.position;
+        if (!equipped && distanceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.E) && !slotFull) PickUp();
+
+        //Drop if equipped and "Q" is pressed
+        if (equipped && Input.GetKeyDown(KeyCode.Q)) Drop();
+    }
+
+    private void PickUp()
+    {
+        equipped = true;
+        slotFull = true;
+
+        //Make weapon a child of the camera and move it to default position
+        transform.SetParent(gunContainer);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localScale = Vector3.one;
+
+        //Make Rigidbody kinematic and BoxCollider a trigger
+        rb.isKinematic = true;
+        coll.isTrigger = true;
+
+        player.GetComponent<PlayerShooting>().weapon = weapon;
+        player.GetComponent<PlayerShooting>().weaponComponent = weapon.GetComponent<Gun>();
+
+        //Enable script
+        gunScript.enabled = true;
+    }
+
+    private void Drop()
+    {
+        equipped = false;
+        slotFull = false;
+
+        //Set parent to null
+        transform.SetParent(null);
+
+        //Make Rigidbody not kinematic and BoxCollider normal
+        rb.isKinematic = false;
+        coll.isTrigger = false;
+
+        //Gun carries momentum of player
+        rb.velocity = player.GetComponent<Rigidbody>().velocity;
+
+        //AddForce
+        rb.AddForce(fpsCam.forward * dropForwardForce, ForceMode.Impulse);
+        rb.AddForce(fpsCam.up * dropUpwardForce, ForceMode.Impulse);
+        //Add random rotation
+        float random = Random.Range(-1f, 1f);
+        rb.AddTorque(new Vector3(random, random, random) * 10);
+
+        //Disable script
+        gunScript.enabled = false;
+    }
+}
