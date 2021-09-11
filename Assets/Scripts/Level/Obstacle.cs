63d05a8f5fad4a7733c253a88dff6dbd47e5d771
Fishu99 <lukasz.sygula08@gmail.com>
2021-09-11 20:14:20 +0200

    Documentation of the Obstacle class

--- Assets/Scripts/Level/Obstacle.cs
+++ Assets/Scripts/Level/Obstacle.cs
@@ -2,10 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class which defines information about an obstacle in the room.
+/// </summary>
 public class Obstacle : MonoBehaviour
 {
+    /// <summary>
+    /// Type of the obstacle.
+    /// </summary>
     [SerializeField] private int obstacleType;
 
+    /// <summary>
+    /// Access to obstacleType variable.
+    /// </summary>
+    /// <returns>obstacleType variable</returns>
     public int getType() {
         return obstacleType;
     }
